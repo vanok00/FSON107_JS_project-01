d131933234bdce10b67df91050051bf1b0e5a158
kmositua <kmositua@gmail.com>
2024-08-19 18:34:41 +0100

    fix

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -15,12 +15,5 @@ document.querySelectorAll('.ac-trigger').forEach(button => {
   button.addEventListener('click', () => {
     const ac = button.closest('.ac');
     ac.classList.toggle('is-active');
-
-    const panel = ac.querySelector('.ac-panel');
-    if (ac.classList.contains('is-active')) {
-      panel.style.maxHeight = panel.scrollHeight + 'px';
-    } else {
-      panel.style.maxHeight = 0;
-    }
   });
 });
