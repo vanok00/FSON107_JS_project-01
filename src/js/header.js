d54eb86f89579460902c9c2a76c8438a30520da2
Vanok <vanok008@ukr.net>
2024-08-19 16:06:55 +0300

    worktogether

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,10 @@
 const button_open = document.querySelector('.header-menu-open-btn');
 const button_close = document.querySelector('.mob-menu-btn');
-const menu = document.querySelector('.mobile-menu-overlay');
+const menu = document.querySelector('.mob-menu');
 
 button_open.addEventListener('click', () => {
-    menu.classList.add('hidden');
+    menu.classList.add('is-open');
 });
 button_close.addEventListener('click', () => {
-    menu.classList.remove('hidden');
+    menu.classList.remove('is-open');
 });
